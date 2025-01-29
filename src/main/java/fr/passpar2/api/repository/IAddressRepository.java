f78af5240979ae044901dc8dc6acd9833680e65a
THOMAS LEMAIRE <thomas.lemaire@iut.local>
2025-01-29 08:57:42 +0100

    Fix IAddressRepository

--- src/main/java/fr/passpar2/api/repository/IAddressRepository.java
+++ src/main/java/fr/passpar2/api/repository/IAddressRepository.java
@@ -6,10 +6,10 @@ import org.springframework.data.mongodb.repository.Query;
 
 public interface IAddressRepository extends MongoRepository<AddressDao, String> {
 
-    @Query("{ 'id_customer' : ?0 }")
+    @Query("{ 'id_customer' : { $eq: ?0 } }")
     AddressDao findAddressByCustomerId(int customerId);
 
-    @Query("{ 'id_user' : ?0 }")
+    @Query("{ 'id_user' : { $eq: ?0 } }")
     AddressDao findAddressByUserId(int userId);
 
 }
