7537a751c4187fb39ff983f7fba57edd1f10fbf2
THOMAS LEMAIRE <thomas.lemaire@iut.local>
2025-02-03 16:10:45 +0100

    Fix AddressService

--- src/main/java/fr/passpar2/api/model/AddressDto.java
+++ src/main/java/fr/passpar2/api/model/AddressDto.java
@@ -67,7 +67,6 @@ public class AddressDto {
         this.city = city;
     }
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     public Integer getPostalCode() {
         return postalCode;
     }
