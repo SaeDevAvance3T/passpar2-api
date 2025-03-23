bbd38577c8b046317b027ba6343b55f8afe633ca
thomasIemaire <thomas.lmre14@gmail.com>
2025-03-23 18:20:23 +0100

    fix refactor v2

--- src/main/java/fr/passpar2/api/routes/itinerary/ItineraryController.java
+++ src/main/java/fr/passpar2/api/routes/itinerary/ItineraryController.java
@@ -1,9 +1,16 @@
 package fr.passpar2.api.routes.itinerary;
 
 import fr.passpar2.api.response.ApiResponse;
+import fr.passpar2.api.routes.customer.CustomerDao;
+import fr.passpar2.api.routes.customer.CustomerService;
+import fr.passpar2.api.routes.customer.dto.CustomerBaseDto;
+import fr.passpar2.api.routes.customer.dto.CustomerDto;
 import fr.passpar2.api.routes.itinerary.dto.ItineraryBaseDto;
 import fr.passpar2.api.routes.itinerary.dto.ItineraryDto;
 import fr.passpar2.api.routes.itinerary.dto.ItineraryRequestDto;
+import fr.passpar2.api.routes.user.UserDao;
+import fr.passpar2.api.routes.user.UserService;
+import fr.passpar2.api.routes.user.dto.UserBaseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,8 +23,13 @@ import java.util.List;
 public class ItineraryController {
 
     private final ItineraryService itineraryService;
+    private final UserService userService;
+    private final CustomerService customerService;
 
-    public ItineraryController(ItineraryService itineraryService) {this.itineraryService = itineraryService;}
+    public ItineraryController(ItineraryService itineraryService, UserService userService, CustomerService customerService) {this.itineraryService = itineraryService;
+        this.userService = userService;
+        this.customerService = customerService;
+    }
 
     @GetMapping()
     public ResponseEntity<ApiResponse<List<ItineraryBaseDto>>> getAllItineraries(){
@@ -52,6 +64,17 @@ public class ItineraryController {
 
         ItineraryDao itineraryCreated = itineraryService.createUserItinerary(userId, request);
         ItineraryDto itinerary = new ItineraryDto(itineraryCreated);
+
+        UserDao itineraryUserFound = userService.getUserById(userId);
+        UserBaseDto itineraryUser = new UserBaseDto(itineraryUserFound);
+        itinerary.setUser(itineraryUser);
+
+        List<CustomerDao> itineraryCustomersFound = customerService.getCustomersByItineraryId(itinerary.getId());
+        List<CustomerBaseDto> itineraryCustomers = new ArrayList<>();
+        for (CustomerDao customer: itineraryCustomersFound) {
+            itineraryCustomers.add(new CustomerBaseDto(customer));
+        }
+        itinerary.setCustomersToVisit(itineraryCustomers);
 
         ApiResponse<ItineraryDto> response = new ApiResponse<>(itinerary, HttpStatus.CREATED);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
